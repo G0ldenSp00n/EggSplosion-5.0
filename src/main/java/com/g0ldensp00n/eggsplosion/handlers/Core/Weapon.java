93a2531cad2182f4f9a238c481e6a1f43f8d9f89
G0ldenSp00n <hayfithyan@gmail.com>
2026-02-10 22:43:37 -0800

    feat: update Hoe Balance

--- src/main/java/com/g0ldensp00n/eggsplosion/handlers/Core/Weapon.java
+++ src/main/java/com/g0ldensp00n/eggsplosion/handlers/Core/Weapon.java
@@ -1,6 +1,8 @@
 package com.g0ldensp00n.eggsplosion.handlers.Core;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+
 import java.util.*;
 import org.bukkit.Sound;
 import org.bukkit.entity.Egg;
@@ -9,6 +11,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffectType;
@@ -41,31 +44,31 @@ public class Weapon implements Listener {
           switch (playerInteractEvent.getItem().getType()) {
             case NETHERITE_HOE:
               playerInteractEvent.setCancelled(true);
-              launchWeapon(player, 2, 15, 15, 4);
+              launchWeapon(player, 3f, 15, 15, 4, Material.EGG);
               break;
             case DIAMOND_HOE:
               playerInteractEvent.setCancelled(true);
-              launchWeapon(player, 2, 3, 4.2f, 3);
+              launchWeapon(player, 1.5f, 3, 4.2f, 3, Material.EGG);
               break;
             case GOLDEN_HOE:
               playerInteractEvent.setCancelled(true);
-              launchWeapon(player, 2, 2.8f, 0.8875f, 1);
+              launchWeapon(player, 4.5f, 1.25f, 0.8875f, 1, Material.EGG);
               break;
             case IRON_HOE:
               playerInteractEvent.setCancelled(true);
-              launchWeapon(player, 2, 2.6f, 0.7f, 1);
+              launchWeapon(player, 1.8f, 2.6f, 0.7f, 1, Material.EGG);
               break;
             case COPPER_HOE:
               playerInteractEvent.setCancelled(true);
-              launchWeapon(player, 2, 2.5f, 0.6f, 1);
+              launchWeapon(player, 2f, 2.5f, 0.6f, 1, Material.EGG);
               break;
             case STONE_HOE:
               playerInteractEvent.setCancelled(true);
-              launchWeapon(player, 2, 2.4f, 2.5f, 2);
+              launchWeapon(player, 1.5f, 2.4f, 2.5f, 2, Material.EGG);
               break;
             case WOODEN_HOE:
               playerInteractEvent.setCancelled(true);
-              launchWeapon(player, 2, 2, 0.05f, 1);
+              launchWeapon(player, 1.75f, 1.25f, 0.15f, 0, Material.EGG);
               break;
             default:
               break;
@@ -78,8 +81,8 @@ public class Weapon implements Listener {
     }
   }
 
-  private void launchWeapon(Player player, int velocityMultiplier, float explosionPower, double reloadTime,
-      int rocket_jump_power) {
+  private void launchWeapon(Player player, float velocityMultiplier, float explosionPower, double reloadTime,
+      int rocket_jump_power, Material eggType) {
     reloadTime = reloadTime * 20;
     Long playerReloadTime = reloadingPlayerTimes.get(player.getUniqueId().toString());
     if (playerReloadTime == null || playerReloadTime <= System.currentTimeMillis()) {
@@ -89,7 +92,7 @@ public class Weapon implements Listener {
     }
 
     if (!reloadingPlayers.contains(player)) {
-      fireWeapon(player, velocityMultiplier, explosionPower, rocket_jump_power);
+      fireWeapon(player, velocityMultiplier, explosionPower, rocket_jump_power, eggType);
       reloadingPlayers.add(player);
       xpLoading(player, reloadTime);
     }
@@ -114,9 +117,27 @@ public class Weapon implements Listener {
     }.runTaskTimer(this.plugin, 0, 1);
   }
 
-  private void fireWeapon(Player player, int velocityMultiplier, float explosionSize, int rocket_jump_power) {
+  private void fireWeapon(Player player, float velocityMultiplier, float explosionSize, int rocket_jump_power,
+      Material eggType) {
     player.playSound(player.getLocation(), Sound.ENTITY_CHICKEN_EGG, 0.2f, 1f);
     Egg egg = player.launchProjectile(Egg.class);
+
+    if (eggType != null) {
+      egg.setItem(new ItemStack(eggType));
+    } else {
+      Random random = new Random();
+      switch (random.nextInt(3)) {
+        case 0:
+          egg.setItem(new ItemStack(Material.EGG));
+          break;
+        case 1:
+          egg.setItem(new ItemStack(Material.BLUE_EGG));
+          break;
+        case 2:
+          egg.setItem(new ItemStack(Material.BROWN_EGG));
+          break;
+      }
+    }
     egg.setCustomName(player.getUniqueId() + " / " + explosionSize);
     egg.setVelocity(egg.getVelocity().subtract(player.getVelocity()));
     egg.setVelocity(egg.getVelocity().multiply(velocityMultiplier));
